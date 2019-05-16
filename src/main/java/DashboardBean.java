60a1ede43c2b141b9f13e63f4108fce2b4afb308
Mateusz <matteuszz20@gmail.com>
2019-05-16 11:43:48 +0200

    Add DashboardBean and simple User model

---
+++ src/main/java/DashboardBean.java
@@ -0,0 +1,9 @@
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+
+@ManagedBean(name = "DashboardBean")
+@SessionScoped
+public class DashboardBean {
+
+}
---
+++ src/main/java/Models/User.java
@@ -0,0 +1,8 @@
+package Models;
+
+public class User {
+    private long id;
+    private String firstName;
+    private String lastName;
+
+}

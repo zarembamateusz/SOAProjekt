788b40fb4abb78ed816ee222534a956027da0b36
Mateusz <matteuszz20@gmail.com>
2019-05-19 12:32:20 +0200

    Add zone info to dashboard with hardcode test data about zone

--- src/main/java/DashboardBean.java
+++ src/main/java/DashboardBean.java
@@ -1,9 +1,32 @@
+import Models.Zone;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @ManagedBean(name = "DashboardBean")
 @SessionScoped
 public class DashboardBean {
+    //TODO delete test data and configure other data source
+    private List<Zone> zoneList = new ArrayList<Zone>(){{
+        add(new Zone(1,15,15));
+        add(new Zone(2,20,18));
+        add(new Zone(3,53,50));
+        add(new Zone(4,60,52));
+    }};
 
+    public List<Zone> getZoneList() {
+        return new ArrayList<Zone>(){{
+            add(new Zone(1,15,15));
+            add(new Zone(2,20,18));
+            add(new Zone(3,53,50));
+            add(new Zone(4,60,52));
+        }};
+    }
+
+    public void setZoneList(List<Zone> zoneList) {
+        this.zoneList = zoneList;
+    }
 }
---
+++ src/main/java/Models/Zone.java
@@ -0,0 +1,38 @@
+package Models;
+
+public class Zone {
+    private long id;
+    private int takenSeat;
+    private int purchasedTickets;
+
+    public long getId() {
+        return id;
+    }
+
+    public Zone(long id, int takenSeat, int purchasedTickets) {
+        this.id = id;
+        this.takenSeat = takenSeat;
+        this.purchasedTickets = purchasedTickets;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+
+    }
+
+    public int getTakenSeat() {
+        return takenSeat;
+    }
+
+    public void setTakenSeat(int takenSeat) {
+        this.takenSeat = takenSeat;
+    }
+
+    public int getPurchasedTickets() {
+        return purchasedTickets;
+    }
+
+    public void setPurchasedTickets(int purchasedTickets) {
+        this.purchasedTickets = purchasedTickets;
+    }
+}

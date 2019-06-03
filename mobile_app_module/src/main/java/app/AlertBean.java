768f7f65653a10f6a5bd3711efb821f49dcd5892
Mateusz <matteuszz20@gmail.com>
2019-06-03 09:10:02 +0200

    Add alert view

---
+++ mobile_app_module/src/main/java/app/AlertBean.java
@@ -0,0 +1,43 @@
+package app;
+
+import jms.Event;
+import jms.EventType;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@ManagedBean(name = "AlertBean")
+@SessionScoped
+public class AlertBean {
+
+
+    private List<Event> eventList = new ArrayList<>();
+    private List<Event> filtredEventList = new ArrayList<>();
+    private List<EventType> eventTypes =new ArrayList<EventType>(Arrays.asList(EventType.values()));
+
+    public List<EventType> getEventTypes() {
+        return eventTypes;
+    }
+
+
+    public List<Event> getFiltredEventList() {
+        return filtredEventList;
+    }
+
+    public void setFiltredEventList(List<Event> filtredEventList) {
+        this.filtredEventList = filtredEventList;
+    }
+
+
+    public List<Event> getEventList() {
+        return eventList;
+    }
+
+    public void setEventList(List<Event> eventList) {
+        this.eventList = eventList;
+    }
+
+}

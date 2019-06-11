7f8dfb7bc3b762a7a13ef57f9eca3568da309a24
Mateusz <matteuszz20@gmail.com>
2019-06-11 15:51:01 +0200

    Add autorizati

--- main_module/implementation/src/main/java/api/soap/ZoneSoapServiceImpl.java
+++ main_module/implementation/src/main/java/api/soap/ZoneSoapServiceImpl.java
@@ -1,9 +1,12 @@
 package api.soap;
 
 import jms.Event;
+import jms.EventType;
 import jms.service.JMSService;
+import models.CarPlace;
 import models.Zone;
 import models.service.ZoneService;
+import service.IntervalTaskManager;
 
 import javax.ejb.EJB;
 import javax.jws.WebMethod;
@@ -28,7 +31,30 @@ public class ZoneSoapServiceImpl implements ZoneSoapService {
     @WebMethod
     @Override
     public void action(Event event) {
-        topic.sendTopic(event);
+        IntervalTaskManager.startIntervalTask();
+        Zone zone = zoneService.findById(event.getZoneId());
+        CarPlace carPlace = null;
+        for(CarPlace cp :zone.getPlaces())
+            if(cp.getId().equals(event.getCarPlaceId()))
+                carPlace = cp;
+        if(carPlace == null) return;
+        if(event.getType()==EventType.CAR_IN) {
+            carPlace.setCode("1");
+            //TUTAJ TRZEBA WSTAWIC TICKET DO BAZY TAKI NA 5 MIN
+
+        }else
+            carPlace.setCode("0");
+
+        //tutaj trzeba zrobic update carPlace na bazie
+
+        
+        
+
+
+
+
+//
+//        topic.sendTopic(event);
     }
 
     @Override
---
+++ main_module/implementation/src/main/java/service/IntervalTaskManager.java
@@ -0,0 +1,48 @@
+package service;
+
+import models.CarPlace;
+import models.Zone;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+class MyTask extends TimerTask
+{
+    public static int i = 0;
+    public void run()
+    {
+        ZoneServiceImpl zs = new ZoneServiceImpl();
+        for(Zone z : zs.getAll()) {
+            for (CarPlace cp :z.getPlaces()) {
+                Date date = new Date();
+                long time = date.getTime();//time to milisecundy
+                Timestamp ts = new Timestamp(time);
+                if (cp.getCurrentTicket().getEndTime().isAfter(LocalDateTime.now())) {
+
+                    //tutaj insertujemy event do bazy bo zaszło zdarzenie nieporzadane
+                    //tutaj wysyłamy topic powiadmiajacy o nowym evencie
+                }
+            }
+        }
+
+    }
+}
+
+public class IntervalTaskManager {
+
+    private static boolean isCalled = false;
+    private static int time = 30_000;;
+
+
+    public static void startIntervalTask(){
+        if(isCalled) return;
+        isCalled = true;
+        Timer timer = new Timer();
+        TimerTask task = new MyTask();
+
+        timer.schedule(task, 0, time);
+    }
+}
